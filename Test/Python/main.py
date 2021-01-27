b7bac9dcddde56a48a1c342f4735c953d4845bc4
Qzername <defens123@wp.pl>
2021-01-27 13:08:48 +0100

    First Commit

---
+++ Test/Python/main.py
@@ -0,0 +1,20 @@
+from vulcan import Vulcan;
+import json;
+
+with open('./cert.json') as f:
+    certificate = json.load(f);
+
+client = Vulcan(certificate);
+
+for student in client.get_students():
+    if student.name == "Hubert Szymik":
+        client.set_student(student);
+        break;
+
+final = "";
+
+for grade in client.get_grades():
+    final += grade.teacher.name + " " + grade.subject.name + " " + grade.content + "|";
+
+
+print(final);
