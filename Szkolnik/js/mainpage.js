7d9a72b77b31c114ac2957fddaf84e531d3968e4
Qzername <defens123@wp.pl>
2021-02-04 01:04:19 +0100

    New mainpage design p1

--- Szkolnik/js/mainpage.js
+++ Szkolnik/js/mainpage.js
@@ -1,12 +1,30 @@
 api = "https://localhost:5001";
 var token = "";
 
-window.onload = async function() {
+window.onload = async function() 
+{      
+    Timer()
 
+    document.getElementById("logininfo").innerHTML = localStorage.getItem("login");
+    
     await fetch(api+'/api/Accounts/GetVulcanToken/'+localStorage.getItem("login")).then( res => res.json() ).then( json => jsonFile = json ).catch( e => console.log(e) );
     token = `${jsonFile.token}`;
-
 };
+
+function Timer()
+{
+    var today = new Date();
+             
+    var hour = today.getHours();
+    if (hour<10) hour = "0"+hour;
+            
+    var minut = today.getMinutes();
+    if (minut<10) minut = "0"+minut;
+
+    document.getElementById("timer").innerHTML = hour + ":" + minut + " |"
+
+    setTimeout(Timer, 1000);
+}
 
 function Logout()
 {
@@ -49,4 +67,11 @@ async function Marks()
     });
 
     document.getElementById("output").innerHTML = oceny;
+}
+
+function Logout()
+{
+    localStorage.setItem("login", "");
+    localStorage.setItem("password", "");
+    window.location.href = "index.html"
 }
