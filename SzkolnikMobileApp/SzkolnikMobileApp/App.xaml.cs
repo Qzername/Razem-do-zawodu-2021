3ad4137f0e75a47d998efa0dbf51c276e1a7f72f
Qzername <defens123@wp.pl>
2021-02-06 15:36:11 +0100

    First commit with Mobile app

---
+++ SzkolnikMobileApp/SzkolnikMobileApp/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SzkolnikMobileApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+
+    }
+}
